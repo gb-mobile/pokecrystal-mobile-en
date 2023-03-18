8eb9083a4705be4460ef63e5622801782324682b
Damien Doury <damien.doury@hotmail.fr>
2023-03-18 19:32:18 +0100

    Missing link found: the data folder is now fixed.
    TODO: fix non-game-breaking oversights.

--- engine/link/mystery_gift.asm
+++ engine/link/mystery_gift.asm
@@ -1734,7 +1734,7 @@ StageDataForNameCard:
 	ld a, BANK(sPlayerData)
 	call OpenSRAM
 	ld hl, sPlayerData + wPlayerName - wPlayerData
-	ld bc, NAME_LENGTH
+	ld bc, PLAYER_NAME_LENGTH
 	call CopyBytes
 	ld hl, sPlayerData + wPlayerID - wPlayerData
 	ld bc, 2
